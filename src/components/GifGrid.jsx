c662831af8ec7ffdb57a5589ed12390b493a7a11
Ever Canales <evercanales@gmail.com>
2024-10-19 11:59:43 -0600

    primer commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,17 @@
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+export const GifGrid = ({ category }) => {
+  const { images, isLoading } = useFetchGifs(category);
+  console.log({ images, isLoading });
+
+  return (
+    <>
+      <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+      {images.map((image) => (
+        <GifItem key={image.id} title={image.title} {...image} />
+      ))}
+    </>
+  );
+};
